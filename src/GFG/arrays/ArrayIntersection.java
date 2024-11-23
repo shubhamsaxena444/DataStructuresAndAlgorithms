c7d1453ffbaaf80ccc7b6eb85b473579e414d361
shubham Saxena <shubham@shubhams-MacBook-Air.local>
2024-11-23 15:04:28 +0530

    adding comments

--- src/GFG/arrays/ArrayIntersection.java
+++ src/GFG/arrays/ArrayIntersection.java
@@ -2,28 +2,38 @@ package GFG.arrays;
 
 import java.util.*;
 
+/**
+ * The ArrayIntersection class provides a method to find the intersection of two integer arrays.
+ */
 public class ArrayIntersection {
+
     public static int[] intersect(int[] nums1, int[] nums2) {
-        if(nums1.length==0 || nums2.length == 0){
+        if (nums1.length == 0 || nums2.length == 0) {
             return new int[]{};
         }
-        List<Integer> r = new ArrayList<>();
-        Map<Integer,Integer> hm = new HashMap<>();
-        for(int i =0 ;i<nums1.length;i++)    {
-            hm.put(nums1[i],hm.get(nums1[i])==null?1:hm.get(nums1[i])+1);
 
-        }
-        for(int i =0;i<nums2.length;i++){
-            //find in map, if found and frequency > 0, add in r and dec frequency.
-            if(hm.containsKey(nums2[i]) && hm.get(nums2[i])>0){
-                r.add(nums2[i]);
-                hm.put(nums2[i],hm.get(nums2[i])-1);
+        Map<Integer, Integer> frequencyMap = createFrequencyMap(nums1);
+        List<Integer> result = new ArrayList<>();
+
+        for (int num : nums2) {
+            if (frequencyMap.containsKey(num) && frequencyMap.get(num) > 0) {
+                result.add(num);
+                frequencyMap.put(num, frequencyMap.get(num) - 1);
             }
         }
-        return r.stream().mapToInt(Integer::intValue).toArray();
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private static Map<Integer, Integer> createFrequencyMap(int[] nums) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+        return frequencyMap;
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(intersect(new int[]{4,9,5},new int[]{9,4,9,8,4})));
+        System.out.println(Arrays.toString(intersect(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4})));
     }
 }
